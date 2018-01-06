9941d07f0ec05ec4128090f7bdb63a0542f7297e
saladinid <didien.geonk@gmail.com>
2018-01-07 02:40:18 +0800

    fix(generated): clean up

--- client/development/bootstrap.js
+++ client/development/bootstrap.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "95c09a24649e60eea08e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "fd7e48fe8abe0b801774"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
